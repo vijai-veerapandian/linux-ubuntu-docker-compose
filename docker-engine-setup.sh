65493a7204a46f44ac251a6799d5924c88bf78aa
Vijai <144189831+vijai-veerapandian@users.noreply.github.com>
2025-08-16 06:31:42 -0400

    Update docker-engine-setup.sh

--- docker-engine-setup.sh
+++ docker-engine-setup.sh
@@ -3,9 +3,9 @@
 
 sudo apt-get update
 
-sudo apt-get install ca-certificates curl gnupg lsb-relase
+sudo apt-get install ca-certificates curl gnupg lsb-release
 
-sudo makdir -p /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
